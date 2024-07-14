b13a89487e7b2bbc555589be181bc5f67dc48c85
MattRob92 <164926430+MattRob92@users.noreply.github.com>
2024-07-14 13:11:08 +0100

    Basic Etch-A-Sketch Complete
    Etch Sketch now works as intended but no features.

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const GRIDSIDE = 600;
-let rows = 16;
-let cols = 16;
+let SquaresPerSide = 16;
 
 //Searches the html file for sketch-area and assigns it
 //The height and width of GRIDSIDE
 const SketchArea = document.querySelector("#sketch-area");
-SketchArea.style.width =`${GRIDSIDE}px`;
-SketchArea.style.height = `${GRIDSIDE}px`
+SketchArea.style.width = SketchArea.style.height = `${GRIDSIDE}px`
 
-function changeBackgroundColour() {
+function SetBackgroundColour() {
     this.style.backgroundColor = "black";
 }
 
 
 //add for loop that creates a new div
-// sets the height and width of the div using cols and rows.
+// sets total number of squares & there heught and width
 function CreateGridCells() {
-    for (let i = 0; i< (rows * cols); i++) {
+    const numOfSquares = (SquaresPerSide * SquaresPerSide)
+    const WidthOrHeight = `${(GRIDSIDE / SquaresPerSide)-2}px`;
+
+
+    for (let i = 0; i< numOfSquares; i++) {
         const GridCell = document.createElement("div");
 
-        GridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
-        GridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        GridCell.style.width = GridCell.style.height = WidthOrHeight;
         GridCell.classList.add("cell");
 
         SketchArea.appendChild(GridCell);
 
         //add event for cells we just  created and call changebackgroundcolour function
-        GridCell.addEventListener("mouseover", changeBackgroundColour);
+        GridCell.addEventListener("mouseover", SetBackgroundColour);
     }
 }
 
