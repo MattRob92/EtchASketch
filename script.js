4bb333609a3036b7bd1db2a99f191937f7be5aa6
MattRob92 <164926430+MattRob92@users.noreply.github.com>
2024-07-14 12:11:33 +0100

    Initial sketch area & Cell creation
    Setup new sketch area and create cells is JS to fill in Sketch Area

---
+++ script.js
@@ -0,0 +1,24 @@
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+
+//Searches the html file for sketch-area and assigns it
+//The height and width of GRIDSIDE
+const SketchArea = document.querySelector("#sketch-area");
+SketchArea.style.width =`${GRIDSIDE}px`;
+SketchArea.style.height = `${GRIDSIDE}px`
+
+
+//add for loop that creates a new div
+// sets the height and width of the div using cols and rows.
+function CreateGridCells() {
+    for (let i = 0; i< (row * cols); i++) {
+        const GridCell = document.createElement("div");
+
+        GridCell.style.width = `${(GRIDSIDE / cols) - 2}`;
+        GridCell.style.height = `${(GRIDSIDE / rows) - 2}`;
+        GridCell.classList.add("cell");
+
+        SketchArea.appendChild(GridCell)
+    }
+}
